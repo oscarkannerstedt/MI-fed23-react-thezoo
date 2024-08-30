6b0e52dd4f3c4a12d9bcd1ad665aa7bab633144f
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-08-30 09:36:18 +0200

    Added - styling home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,25 @@
+import startImage from "../assets/zoo.webp";
+import "../styles/home.css"
+
 export const Home = () => {
-    return <>
-        <h1>Startsida....</h1>
-    </>
+    return (
+        <div className="home">
+            <section className="hero">
+                <img src={startImage} alt="zoo animals" className="hero-image" />
+                <div className="hero-text">
+                <h1>Välkommen till Zoo!</h1>
+                <p>Upplev en värld fylld av fantastiska djur och hjälp oss att mata dom.</p>
+                </div>
+            </section>
+
+            {/* <section className="introduction">
+                <h2>Om Vårt Zoo</h2>
+                <p>På vårt zoo kan du utforska och lära dig mer om djur från alla världens hörn. Vi erbjuder
+                en unik upplevelse där du kan komma nära naturen och upptäcka en mängd olika arter, deras
+                livsmiljöer och beteenden. Vårt mål är att främja bevarandet av hotade arter och utbilda
+                våra besökare om djur och miljö.
+                </p>
+            </section> */}
+        </div>
+    );
 };
