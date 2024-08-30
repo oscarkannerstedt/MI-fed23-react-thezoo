aee1632664fcf8780f8f8734af68247362573861
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-08-30 10:17:46 +0200

    Added - styling for footer

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,5 +1,6 @@
 import { NavLink, Outlet } from "react-router-dom";
 import "../styles/header.css";
+import "../styles/footer.css";
 
 export const Layout = () => {
     return <>
@@ -15,6 +16,11 @@ export const Layout = () => {
         <main>
             <Outlet></Outlet>
         </main>
-        <footer></footer>
+        <footer>
+            <div className="footer-content">
+                <p>Kontakta oss: info@zoo.com | Telefon: 123-456 789</p>
+                <p>&copy; 2024 Zoo.</p>
+            </div>
+        </footer>
     </>
 };
