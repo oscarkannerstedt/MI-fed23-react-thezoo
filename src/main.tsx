b68506611c381c518773d3251e60e75506839f4a
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-09-02 17:22:43 +0200

    Added - StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
     <App />
-    </HashRouter>
   </StrictMode>
 )
