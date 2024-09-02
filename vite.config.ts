283d7188f1dc435d7891a9bcc80700b99b8d4a6c
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-09-02 16:33:23 +0200

    Added - deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MI-fed23-react-thezoo/"
+  base: "/MI-fed23-react-thezoo/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 })
